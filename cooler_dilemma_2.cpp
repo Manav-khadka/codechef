f8d9b2ee557e0146aa6d91463890225c9bbd6a8b
manav <manavkhadka2004@gmail.com>
2022-04-27 22:46:30 +0530

    second commit

---
+++ cooler_dilemma_2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n],b[n],m[n];
+    for (int i=0;i<n;i++){
+        cin>>a[i]>>b[i];  //input
+        m[i]=(b[i]/a[i]);
+
+    }
+    for (int i = 0; i <n; i++)
+    {
+        cout<<m[i]<<" ";
+    }
+    
+    
+
+}

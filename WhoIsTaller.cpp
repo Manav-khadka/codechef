43eda432a617cd3645e8771da131ba925db68b1c
manav <manavkhadka2004@gmail.com>
2022-04-28 21:39:19 +0530

    today first commit

---
+++ WhoIsTaller.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n],b[n];
+    for (int i=0;i<n;i++) {
+        
+        cin>>a[i]>>b[i];
+    }
+    for(int i=0;i<n;i++){
+        if(a[i]>b[i]){
+            cout<<"A"<<"\n";
+        }
+        else{
+            cout<<"B"<<"\n";
+        }
+    }
+}

bd09023c8951ca65c5a20b6d67989914a7a7260c
manav <manavkhadka2004@gmail.com>
2022-04-29 19:26:00 +0530

    day_1 second

---
+++ RCB_play.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,N;
+    cin>>n;
+    int x[n],y[n],z[n];
+    for (int i = 0; i <n; i++)
+    {
+        cin>>x[i]>>y[i]>>z[i];
+    }
+    for (int i = 0; i <n; i++)
+    {
+        if ((x[i]+(2*z[i]))>y[i])
+        {
+            cout<<"YES"<<endl;
+           
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+        
+     
+    }
+    
+}
